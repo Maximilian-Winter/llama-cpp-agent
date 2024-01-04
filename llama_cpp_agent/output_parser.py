7ff5eb9e305329b26c9f23917d1c96c111332f7b
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-01-04 02:55:31 +0100

    Update output_parser.py

--- llama_cpp_agent/output_parser.py
+++ llama_cpp_agent/output_parser.py
@@ -30,7 +30,7 @@ def parse_json_response_with_file_string(file_string):
     # Remove the last line Markdown code block marker
     response_lines.pop(-1)
     # Combine lines into a single string
-    file_string_content = sanitize_json_string("\n".join(response_lines))
+    file_string_content = "\n".join(response_lines)
     json_object = parse_json_response(response)
 
     return json_object, file_string_content

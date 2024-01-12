5de50c0b532b5c3c8c81ea1061ee8e5bff925664
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-01-12 07:01:29 +0100

    Update example_manual_function_calling.py

--- examples/example_manual_function_calling.py
+++ examples/example_manual_function_calling.py
@@ -7,6 +7,7 @@ from llama_cpp import Llama, LlamaGrammar
 from llama_cpp_agent.llm_agent import LlamaCppAgent
 from llama_cpp_agent.gbnf_grammar_generator.gbnf_grammar_from_pydantic_models import \
     generate_gbnf_grammar_and_documentation, create_dynamic_model_from_function
+from llama_cpp_agent.providers.llama_cpp_server_provider import LlamaCppServerLLMSettings
 
 
 def calculate_a_to_the_power_b(a: Union[int | float], b: Union[int | float]):
@@ -18,26 +19,14 @@ DynamicSampleModel = create_dynamic_model_from_function(calculate_a_to_the_power
 grammar, documentation = generate_gbnf_grammar_and_documentation([DynamicSampleModel], root_rule_class="function",
                                                                  root_rule_content="params")
 
-llama_grammar = LlamaGrammar.from_string(grammar)
-
-main_model = Llama(
-    "../../gguf-models/dpopenhermes-7b-v2.Q8_0.gguf",
-    n_gpu_layers=35,
-    f16_kv=True,
-    use_mlock=False,
-    embedding=False,
-    n_threads=8,
-    n_batch=1024,
-    n_ctx=8192,
-    last_n_tokens_size=1024,
-    verbose=False,
-    seed=-1,
+main_model = LlamaCppServerLLMSettings(
+    completions_endpoint_url="http://127.0.0.1:8080/completion"
 )
 
 llama_cpp_agent = LlamaCppAgent(main_model, debug_output=True,
                                 system_prompt="You are an advanced AI, tasked to generate JSON objects for function calling.\n\n" + documentation)
 
-response = llama_cpp_agent.get_chat_response("a= 5, b = 42", temperature=0.15, grammar=llama_grammar)
+response = llama_cpp_agent.get_chat_response("a= 5, b = 42", temperature=0.15, grammar=grammar)
 
 function_call = json.loads(response)
 

c0bd3c3323b0f7221ae736797bb567e15dd02632
Ruben Hohndorf <ruben.hohndorf@gmail.com>
2024-03-29 02:34:31 +0100

    Removed outdated function calling agent example

--- examples/05_Agents/function_calling_agent.py
+++ examples/05_Agents/function_calling_agent.py
@@ -12,40 +12,12 @@ from llama_cpp_agent.function_calling_agent import FunctionCallingAgent
 from llama_cpp_agent.providers.llama_cpp_endpoint_provider import LlamaCppEndpointSettings, LlamaCppGenerationSettings
 
 
-# llama-cpp-agent supports type hinted function definitions for function calling.
-# Write to file function that can be used by the agent. Docstring will be used in system prompt.
-def write_to_file(chain_of_thought: str, file_path: str, file_content: str):
-    """
-    Write file to the user filesystem.
-    :param chain_of_thought: Your chain of thought while writing the file.
-    :param file_path: The file path includes the filename and file ending.
-    :param file_content: The actual content to write.
-    """
-    print(chain_of_thought)
-    with open(file_path, mode="w", encoding="utf-8") as file:
-        file.write(file_content)
-    return f"File {file_path} successfully written."
-
-
-# Read file function that can be used by the agent. Docstring will be used in system prompt.
-def read_file(file_path: str):
-    """
-    Read file from the user filesystem.
-    :param file_path: The file path includes the filename and file ending.
-    :return: File content.
-    """
-    output = ""
-    with open(file_path, mode="r", encoding="utf-8") as file:
-        output = file.read()
-    return f"Content of file '{file_path}':\n\n{output}"
-
-
 def get_current_datetime(output_format: Optional[str] = None):
     """
     Get the current date and time in the given format.
 
     Args:
-        output_format: format string for the date and time. Defaults to '%Y-%m-%d %H:%M:%S'
+         output_format: formatting string for the date and time, defaults to '%Y-%m-%d %H:%M:%S'
     """
     if output_format is None:
         output_format = '%Y-%m-%d %H:%M:%S'
@@ -66,9 +38,9 @@ class Calculator(BaseModel):
     """
     Perform a math operation on two numbers.
     """
-    number_one: Any = Field(..., description="First number.")
+    number_one: Union[int, float] = Field(..., description="First number.")
     operation: MathOperation = Field(..., description="Math operation to perform.")
-    number_two: Any = Field(..., description="Second number.")
+    number_two: Union[int, float] = Field(..., description="Second number.")
 
     def run(self):
         if self.operation == MathOperation.ADD:
@@ -88,13 +60,13 @@ class Calculator(BaseModel):
 def get_current_weather(location, unit):
     """Get the current weather in a given location"""
     if "London" in location:
-        return json.dumps({"location": "London", "temperature": "42", "unit": unit.value})
+        return f"Weather in {location}: {42}° {unit.value}"
     elif "New York" in location:
-        return json.dumps({"location": "New York", "temperature": "24", "unit": unit.value})
+        return f"Weather in {location}: {24}° {unit.value}"
     elif "North Pole" in location:
-        return json.dumps({"location": "North Pole", "temperature": "-42", "unit": unit.value})
+        return f"Weather in {location}: {-42}° {unit.value}"
     else:
-        return json.dumps({"location": location, "temperature": "unknown"})
+        return f"Weather in {location}: unknown"
 
 
 # Here is a function definition in OpenAI style
@@ -127,7 +99,7 @@ def send_message_to_user_callback(message: str):
     print(message)
 
 
-generation_settings = LlamaCppGenerationSettings(temperature=0.65, stream=True)
+generation_settings = LlamaCppGenerationSettings(temperature=0, stream=True)
 
 # Can be saved and loaded like that:
 # generation_settings.save("generation_settings.json")
@@ -135,8 +107,6 @@ generation_settings = LlamaCppGenerationSettings(temperature=0.65, stream=True)
 main_model = LlamaCppEndpointSettings(
     "http://localhost:8080/completion"
 )
-
-
 function_call_agent = FunctionCallingAgent(
     # Can be lama-cpp-python Llama class, llama_cpp_agent.llm_settings.LlamaLLMSettings class or llama_cpp_agent.providers.llama_cpp_server_provider.LlamaCppServerLLMSettings.
     main_model,
@@ -145,7 +115,7 @@ function_call_agent = FunctionCallingAgent(
     # A tuple of the OpenAI style function definitions and the actual functions
     open_ai_functions=(tools, tool_functions),
     # Just a list of type hinted functions for normal Python functions
-    python_functions=[write_to_file, read_file, get_current_datetime],
+    python_functions=[get_current_datetime],
     # Just a list of pydantic types
     pydantic_functions=[Calculator],
     # Callback for receiving messages for the user.
@@ -153,8 +123,9 @@ function_call_agent = FunctionCallingAgent(
     # Set to true to allow parallel function calling
     allow_parallel_function_calling=True,
     debug_output=True)
-
-
-user_input = 'Solve the following calculations: 42 * 42, 24 * 24, 5 * 5'
+user_input = '''Format the answer clearly: Get the date and time, get the current weather in celsius in London and solve the following calculation: 42 * 42. And return the result.'''
+function_call_agent.generate_response(user_input)
+function_call_agent.save("function_calling_agent.json")
+user_input = input(">")
 function_call_agent.generate_response(user_input)
 function_call_agent.save("function_calling_agent.json")

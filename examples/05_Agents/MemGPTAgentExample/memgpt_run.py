c6b949f79dfc42273b4b1e5f1a9956ba3ffdd2bc
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-03-29 06:47:44 +0100

    Update memgpt_run.py

--- examples/05_Agents/MemGPTAgentExample/memgpt_run.py
+++ examples/05_Agents/MemGPTAgentExample/memgpt_run.py
@@ -5,7 +5,7 @@ from memgpt_agent import MemGptAgent
 main_model = LlamaCppEndpointSettings(completions_endpoint_url="http://127.0.0.1:8080/completion")
 
 mem_gpt_agent = MemGptAgent(main_model, debug_output=True, core_memory_file="core_memory.json",
-                            #event_queue_file="event_queue.json",
+                            event_queue_file="event_queue.json",
                             messages_formatter_type=MessagesFormatterType.CHATML
                             )
 

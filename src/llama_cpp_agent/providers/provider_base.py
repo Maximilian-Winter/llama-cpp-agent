eb32535a39502edb69330ffaca04494f5fbbe7a0
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-05-01 16:31:29 +0200

    Update provider_base.py

--- src/llama_cpp_agent/providers/provider_base.py
+++ src/llama_cpp_agent/providers/provider_base.py
@@ -37,6 +37,9 @@ class LLMGenerationSettings(BaseModel):
         else:
             raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
 
+    def as_dict(self) -> Dict[str, Any]:
+        return self.get_settings_dict()
+
 
 class LLMProviderBase(ABC):
 
@@ -58,39 +61,64 @@ class LLMProviderBase(ABC):
 
 
 class LlamaCppServerProvider(LLMProviderBase):
-    def __init__(self, server_address: str, api_key: str = None):
+    def __init__(self, server_address: str, api_key: str = None, llama_cpp_python_server: bool = False):
         self.server_address = server_address
-        self.server_completion_endpoint = self.server_address + "/completion"
+        if llama_cpp_python_server:
+            self.server_completion_endpoint = self.server_address + "/v1/engines/copilot-codex/completions"
+        else:
+            self.server_completion_endpoint = self.server_address + "/completion"
+
         self.server_chat_completion_endpoint = self.server_address + "/v1/chat/completions"
-        self.server_tokenize_endpoint = self.server_address + "/tokenize"
+        if llama_cpp_python_server:
+            self.server_tokenize_endpoint = self.server_address + "/extras/tokenize"
+        else:
+            self.server_tokenize_endpoint = self.server_address + "/tokenize"
         self.api_key = api_key
+        self.llama_cpp_python_server = llama_cpp_python_server
 
     def get_default_generation_settings(self) -> LLMGenerationSettings:
         default_settings = LLMGenerationSettings()
-        dic = {
-            "temperature": 0.8,
-            "top_k": 40,
-            "top_p": 0.95,
-            "min_p": 0.05,
-            "n_predict": -1,
-            "n_keep": 0,
-            "stream": False,
-            "stop_sequences": [],
-            "tfs_z": 1.0,
-            "typical_p": 1.0,
-            "repeat_penalty": 1.1,
-            "repeat_last_n": -1,
-            "penalize_nl": False,
-            "presence_penalty": 0.0,
-            "frequency_penalty": 0.0,
-            "penalty_prompt": None,
-            "mirostat_mode": 0,
-            "mirostat_tau": 5.0,
-            "mirostat_eta": 0.1,
-            "seed": -1,
-            "ignore_eos": False
-        }
-
+        if self.llama_cpp_python_server:
+            dic = {
+                "temperature": 0.8,
+                "top_k": 40,
+                "top_p": 0.95,
+                "min_p": 0.05,
+                "max_tokens": -1,
+                "stream": False,
+                "stop_sequences": [],
+                "repeat_penalty": 1.1,
+                "presence_penalty": 0.0,
+                "frequency_penalty": 0.0,
+                "mirostat_mode": 0,
+                "mirostat_tau": 5.0,
+                "mirostat_eta": 0.1,
+                "seed": -1
+            }
+        else:
+            dic = {
+                "temperature": 0.8,
+                "top_k": 40,
+                "top_p": 0.95,
+                "min_p": 0.05,
+                "n_predict": -1,
+                "n_keep": 0,
+                "stream": False,
+                "stop_sequences": [],
+                "tfs_z": 1.0,
+                "typical_p": 1.0,
+                "repeat_penalty": 1.1,
+                "repeat_last_n": -1,
+                "penalize_nl": False,
+                "presence_penalty": 0.0,
+                "frequency_penalty": 0.0,
+                "penalty_prompt": None,
+                "mirostat_mode": 0,
+                "mirostat_tau": 5.0,
+                "mirostat_eta": 0.1,
+                "seed": -1,
+                "ignore_eos": False,
+            }
         # Populate the LLMGenerationSettings instance
         for key, value in dic.items():
             default_settings.set_setting(key, value)
@@ -109,19 +137,19 @@ class LlamaCppServerProvider(LLMProviderBase):
         data = copy(settings.as_dict())
         data["prompt"] = prompt
         data["grammar"] = grammar
-        data["mirostat"] = data["mirostat_mode"]
-        data["stop"] = data["stop_sequences"]
-        del data["mirostat_mode"]
-        del data["stop_sequences"]
-        if "samplers" not in data or data["samplers"] is None:
-            data["samplers"] = [
-                "top_k",
-                "tfs_z",
-                "typical_p",
-                "top_p",
-                "min_p",
-                "temperature",
-            ]
+        if not self.llama_cpp_python_server:
+            data["mirostat"] = data.pop("mirostat_mode")
+        data["stop"] = data.pop("stop_sequences")
+        if not self.llama_cpp_python_server:
+            if "samplers" not in data or data["samplers"] is None:
+                data["samplers"] = [
+                    "top_k",
+                    "tfs_z",
+                    "typical_p",
+                    "top_p",
+                    "min_p",
+                    "temperature",
+                ]
         if settings.stream:
             return self.get_response_stream(headers, data, self.server_completion_endpoint)
 
@@ -145,19 +173,19 @@ class LlamaCppServerProvider(LLMProviderBase):
         data = copy(settings.as_dict())
         data["messages"] = messages
         data["grammar"] = grammar
-        data["mirostat"] = data["mirostat_mode"]
-        data["stop"] = data["stop_sequences"]
-        del data["mirostat_mode"]
-        del data["stop_sequences"]
-        if "samplers" not in data or data["samplers"] is None:
-            data["samplers"] = [
-                "top_k",
-                "tfs_z",
-                "typical_p",
-                "top_p",
-                "min_p",
-                "temperature",
-            ]
+        if not self.llama_cpp_python_server:
+            data["mirostat"] = data.pop("mirostat_mode")
+        data["stop"] = data.pop("stop_sequences")
+        if not self.llama_cpp_python_server:
+            if "samplers" not in data or data["samplers"] is None:
+                data["samplers"] = [
+                    "top_k",
+                    "tfs_z",
+                    "typical_p",
+                    "top_p",
+                    "min_p",
+                    "temperature",
+                ]
         if settings.stream:
             return self.get_response_stream(headers, data, self.server_chat_completion_endpoint)
 
@@ -177,7 +205,10 @@ class LlamaCppServerProvider(LLMProviderBase):
             }
         else:
             headers = {"Content-Type": "application/json"}
-        response = requests.post(self.server_tokenize_endpoint, headers=headers, json={"content": prompt})
+        if self.llama_cpp_python_server:
+            response = requests.post(self.server_tokenize_endpoint, headers=headers, json={"input": prompt})
+        else:
+            response = requests.post(self.server_tokenize_endpoint, headers=headers, json={"content": prompt})
         if response.status_code == 200:
             tokens = response.json()["tokens"]
             return tokens
@@ -214,35 +245,3 @@ class LlamaCppServerProvider(LLMProviderBase):
                 print(f"Request failed: {e}")
 
         return generate_text_chunks()
-
-
-class OpenAIProvider(LLMProviderBase):
-    def get_default_settings(self) -> LLMGenerationSettings:
-        default_settings = LLMGenerationSettings()
-        dic = {
-            "max_tokens": 0,
-            "temperature": 0.8,
-            "top_p": 0.95,
-            "min_p": 0.05,
-            "echo": False,
-            "stop_sequences": None,
-            "stream": False,
-            "logprobs": None,
-            "presence_penalty": 0.0,
-            "frequency_penalty": 0.0,
-            "logit_bias": None,
-            "seed": None,
-            "top_k": 40,
-            "repeat_penalty": 1.1,
-            "logit_bias_type": None,
-            "mirostat_mode": 0,
-            "mirostat_tau": 5.0,
-            "mirostat_eta": 0.1,
-            "grammar": None
-        }
-
-        # Populate the LLMGenerationSettings instance
-        for key, value in dic.items():
-            default_settings.set_setting(key, value)
-
-        return default_settings

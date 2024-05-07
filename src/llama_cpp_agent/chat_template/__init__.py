4ab52d4699c58a9d06ec0a359640e3f08a613aed
PΔBLØ ᄃΞ <pabloce@poscye.com>
2024-05-07 15:40:19 -0500

    add: chat template with jinja

---
+++ src/llama_cpp_agent/chat_template/__init__.py
@@ -0,0 +1,17 @@
+from .chat_template import (
+    alpaca_template,
+    amberchat_template,
+    chatml_template,
+    chatqa_template,
+    falcon_template,
+    gemma_template,
+    llama_2_template,
+    llama_3_template,
+    mistral_template,
+    openchat_template,
+    phi_3_template,
+    saiga_template,
+    solar_template,
+    vicuna_template,
+    zephyr_template,
+)

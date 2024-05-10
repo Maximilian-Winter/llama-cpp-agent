e2241d2f4c77f5bfea09896bf2148ab4a2903d0e
Maximilian Winter <maximilian.winter.91@gmail.com>
2024-05-10 13:25:25 +0200

    Update book_dataset_creation.py

--- examples/02_Structured_Output/book_dataset_creation.py
+++ examples/02_Structured_Output/book_dataset_creation.py
@@ -4,13 +4,11 @@ from typing import List
 from pydantic import BaseModel, Field
 
 from llama_cpp_agent.llm_agent import LlamaCppAgent
-from llama_cpp_agent.gbnf_grammar_generator.gbnf_grammar_from_pydantic_models import (
-    generate_gbnf_grammar_and_documentation,
-)
-from llama_cpp_agent.llm_output_settings import LlmStructuredOutputSettings, LlmStructuredOutputType
-from llama_cpp_agent.providers.llama_cpp_server import LlamaCppServerProvider
 
-model = LlamaCppServerProvider("http://127.0.0.1:8080")
+from llama_cpp_agent.llm_output_settings import LlmStructuredOutputSettings, LlmStructuredOutputType
+from llama_cpp_agent.providers.tgi_server import TGIServerProvider
+
+model = TGIServerProvider("http://localhost:8080")
 
 text = """The Feynman Lectures on Physics is a physics textbook based on some lectures by Richard Feynman, a Nobel laureate who has sometimes been called "The Great Explainer". The lectures were presented before undergraduate students at the California Institute of Technology (Caltech), during 1961–1963. The book's co-authors are Feynman, Robert B. Leighton, and Matthew Sands."""
 
@@ -37,7 +35,7 @@ class Book(BaseModel):
     summary: str = Field(..., description="Summary of the book.")
 
 
-output_settings = LlmStructuredOutputSettings.from_pydantic_models([Book], output_type=LlmStructuredOutputType.object_instance)
+output_settings = LlmStructuredOutputSettings.from_pydantic_models([Book], output_type=LlmStructuredOutputType.list_of_objects)
 
 llama_cpp_agent = LlamaCppAgent(
     model,
@@ -45,4 +43,4 @@ llama_cpp_agent = LlamaCppAgent(
     system_prompt="You are an advanced AI, tasked to create JSON database entries for books.\n\n\n" + output_settings.get_llm_documentation(),
 )
 
-print(llama_cpp_agent.get_chat_response(text))
+print(llama_cpp_agent.get_chat_response(text, structured_output_settings=output_settings))

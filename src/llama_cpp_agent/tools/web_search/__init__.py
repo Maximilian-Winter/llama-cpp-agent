931b953c0aca7e44ad9f10429966cdb1afdc91dd
PΔBLØ ᄃΞ <pabloce@poscye.com>
2024-05-30 16:41:29 -0500

    add GoogleWebSearchProvider import

--- src/llama_cpp_agent/tools/web_search/__init__.py
+++ src/llama_cpp_agent/tools/web_search/__init__.py
@@ -1,4 +1,4 @@
 from .tool import WebSearchTool
 from .web_search_interfaces import WebCrawler, WebSearchProvider
 from .default_web_crawlers import TrafilaturaWebCrawler
-from .default_web_search_providers import DDGWebSearchProvider
+from .default_web_search_providers import DDGWebSearchProvider, GoogleWebSearchProvider

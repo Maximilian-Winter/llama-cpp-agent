05325eb7a3c74acb0d2be96bbfc6dede0c8d3d8e
woheller69 <68678880+woheller69@users.noreply.github.com>
2024-07-07 19:24:12 +0200

    Fix GEMMA MessagesFormatter
    Gemma needs another \n to avoid slow processing of follow up prompts, see #54

--- src/llama_cpp_agent/messages_formatter.py
+++ src/llama_cpp_agent/messages_formatter.py
@@ -180,7 +180,7 @@ neural_chat_prompt_markers = {
 gemma_2_prompt_markers = {
     Roles.system: PromptMarkers("""""", """\n\n"""),
     Roles.user: PromptMarkers("""<start_of_turn>user\n""", """<end_of_turn>\n"""),
-    Roles.assistant: PromptMarkers("""<start_of_turn>model\n""", """<end_of_turn>\n"""),
+    Roles.assistant: PromptMarkers("""<start_of_turn>model\n\n""", """<end_of_turn>\n"""),
     Roles.tool: PromptMarkers("", ""),
 }
 code_ds_prompt_markers = {
